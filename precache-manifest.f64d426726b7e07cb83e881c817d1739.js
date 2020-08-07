2dfc081f6499180db700d9a40d781d78357d51cf
moskalyk <morgan.moskalyk@gmail.com>
2020-08-07 00:49:54 -0400

    Updates

---
+++ precache-manifest.f64d426726b7e07cb83e881c817d1739.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bea95fa5e58528cce2147f2100e79ca",
+    "url": "/wiflo/index.html"
+  },
+  {
+    "revision": "8373d738fc677e1c7a51",
+    "url": "/wiflo/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "f05b8ba491b9ab8bdc99",
+    "url": "/wiflo/static/css/main.d1f7efcd.chunk.css"
+  },
+  {
+    "revision": "8373d738fc677e1c7a51",
+    "url": "/wiflo/static/js/2.55567b99.chunk.js"
+  },
+  {
+    "revision": "8f3bd632b462a556351809e821b520b7",
+    "url": "/wiflo/static/js/2.55567b99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f05b8ba491b9ab8bdc99",
+    "url": "/wiflo/static/js/main.7dc07630.chunk.js"
+  },
+  {
+    "revision": "cc1f7cfc88a6ade16834",
+    "url": "/wiflo/static/js/runtime-main.4d2f49da.js"
+  },
+  {
+    "revision": "906bb524256baf47c209ee12685dbc00",
+    "url": "/wiflo/static/media/less_1.906bb524.png"
+  },
+  {
+    "revision": "b474c75fbebc846c5ccaffd67a360e14",
+    "url": "/wiflo/static/media/more_1.b474c75f.png"
+  }
+]);
